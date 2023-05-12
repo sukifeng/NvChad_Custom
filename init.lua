6bd2be9255859ee9fc530e2b75c8e13e17b78f7d
sukifeng <fengning@hotmail.com>
2023-05-12 13:52:02 +0800

    use pwsh

--- init.lua
+++ init.lua
@@ -16,11 +16,11 @@ vim.cmd([[
 "autocmd InsertEnter * :silent :!e:\\Neovim\\tools\\im-select.exe 2052
  autocmd InsertLeave * :silent :!e:\\Neovim\\tools\\im-select.exe 1033
 "autocmd VimLeave * :silent :!e:\\Neovim\\tools\\im-select.exe 2052
- let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
- let &shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';'
- let &shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
- let &shellpipe  = '2>&1 | %%{ "$_" } | Tee-Object %s; exit $LastExitCode'
- set shellquote= shellxquote=
+ " let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
+ "let &shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';'
+ "let &shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
+ "let &shellpipe  = '2>&1 | %%{ "$_" } | Tee-Object %s; exit $LastExitCode'
+ "set shellquote= shellxquote=
 ]])
 
 --自动切换目录
@@ -29,3 +29,5 @@ vim.o.autochdir = true
 vim.o.shiftwidth = 4
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
+
+vim.o.swapfile = false
