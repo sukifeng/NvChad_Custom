1aea1912c5a9cfc233e3473731e62564e006083b
sukifeng <fengning@hotmail.com>
2023-06-06 09:41:21 +0800

    lua format

--- plugins.lua
+++ plugins.lua
@@ -1,4 +1,4 @@
-local overrides = require("custom.configs.overrides")
+local overrides = require "custom.configs.overrides"
 
 ---@type NvPluginSpec[]
 local plugins = {
@@ -25,7 +25,7 @@ local plugins = {
     -- override plugin configs
     {
         "williamboman/mason.nvim",
-        opts = overrides.mason
+        opts = overrides.mason,
     },
 
     {
@@ -42,10 +42,10 @@ local plugins = {
         "hrsh7th/nvim-cmp",
         opts = {
             mapping = {
-                ["<C-k>"] = require "cmp".mapping.select_prev_item(),
-                ["<C-j>"] = require "cmp".mapping.select_next_item(),
-            }
-        }
+                ["<C-k>"] = require("cmp").mapping.select_prev_item(),
+                ["<C-j>"] = require("cmp").mapping.select_next_item(),
+            },
+        },
     },
     -- Install a plugin
     {
@@ -58,56 +58,54 @@ local plugins = {
     {
         "folke/trouble.nvim",
         dependencies = "nvim-tree/nvim-web-devicons",
-        cmd = {"TroubleToggle"},
+        cmd = { "TroubleToggle" },
         init = function()
             require("trouble").setup {
                 -- your configuration comes here
                 -- or leave it empty to use the default settings
                 -- refer to the configuration section below
             }
-            vim.keymap.set("n", "<leader>tx", "<cmd>TroubleToggle<cr>",
-                {silent = true, noremap = true}
+            vim.keymap.set("n", "<leader>tx", "<cmd>TroubleToggle<cr>", { silent = true, noremap = true })
+            vim.keymap.set(
+                "n",
+                "<leader>tw",
+                "<cmd>TroubleToggle workspace_diagnostics<cr>",
+                { silent = true, noremap = true }
             )
-            vim.keymap.set("n", "<leader>tw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
-                {silent = true, noremap = true}
+            vim.keymap.set(
+                "n",
+                "<leader>td",
+                "<cmd>TroubleToggle document_diagnostics<cr>",
+                { silent = true, noremap = true }
             )
-            vim.keymap.set("n", "<leader>td", "<cmd>TroubleToggle document_diagnostics<cr>",
-                {silent = true, noremap = true}
-            )
-            vim.keymap.set("n", "<leader>tl", "<cmd>TroubleToggle loclist<cr>",
-                {silent = true, noremap = true}
-            )
-            vim.keymap.set("n", "<leader>tq", "<cmd>TroubleToggle quickfix<cr>",
-                {silent = true, noremap = true}
-            )
-            vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>",
-                {silent = true, noremap = true}
-            )
+            vim.keymap.set("n", "<leader>tl", "<cmd>TroubleToggle loclist<cr>", { silent = true, noremap = true })
+            vim.keymap.set("n", "<leader>tq", "<cmd>TroubleToggle quickfix<cr>", { silent = true, noremap = true })
+            vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", { silent = true, noremap = true })
             --local actions = require("telescope.actions")
-            local trouble = require("trouble.providers.telescope")
+            local trouble = require "trouble.providers.telescope"
 
-            local telescope = require("telescope")
+            local telescope = require "telescope"
 
             telescope.setup {
                 defaults = {
                     mappings = {
                         i = {
                             ["<c-t>"] = trouble.open_with_trouble,
-                            ["<C-j>"] = require('telescope.actions').move_selection_next,
-                            ["<C-k>"] = require('telescope.actions').move_selection_previous,
+                            ["<C-j>"] = require("telescope.actions").move_selection_next,
+                            ["<C-k>"] = require("telescope.actions").move_selection_previous,
                         },
                         n = { ["<c-t>"] = trouble.open_with_trouble },
                     },
                 },
             }
-        end
+        end,
     },
     {
         "RRethy/vim-illuminate",
         config = function()
             require("illuminate").configure()
         end,
-        lazy = false
+        lazy = false,
     },
     {
         "ahmedkhalf/project.nvim",
@@ -115,24 +113,24 @@ local plugins = {
             require("project_nvim").setup {
                 patterns = { ".git", "*.eww", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
                 silent_chdir = true,
-                detection_methods = {  "pattern","lsp" }, 
+                detection_methods = { "pattern", "lsp" },
 
                 -- your configuration comes here
                 -- or leave it empty to use the default settings
                 -- refer to the configuration section below
             }
-            require("nvim-tree").setup({
+            require("nvim-tree").setup {
                 sync_root_with_cwd = true,
                 respect_buf_cwd = true,
                 update_focused_file = {
                     enable = true,
-                    update_root = true
+                    update_root = true,
                 },
-            })
-            require('telescope').load_extension('projects')
+            }
+            require("telescope").load_extension "projects"
         end,
         event = "VimEnter",
-        cmd = "Telescope projects"
+        cmd = "Telescope projects",
     },
     {
         "nvim-telescope/telescope.nvim",
@@ -203,19 +201,19 @@ local plugins = {
                     "%.ewt",
                     "%.tar.gz",
                 },
-            }
-        }
+            },
+        },
     },
     {
         "nvim-lualine/lualine.nvim",
         -- "Lunarvim/lualine.nvim",
         config = function()
-            require('lualine').setup {
+            require("lualine").setup {
                 options = {
                     icons_enabled = true,
-                    theme = 'auto',
-                    component_separators = { left = '', right = ''},
-                    section_separators = { left = '', right = ''},
+                    theme = "auto",
+                    component_separators = { left = "", right = "" },
+                    section_separators = { left = "", right = "" },
                     disabled_filetypes = {
                         statusline = {},
                         winbar = {},
@@ -227,59 +225,61 @@ local plugins = {
                         statusline = 1000,
                         tabline = 1000,
                         winbar = 1000,
-                    }
+                    },
                 },
                 sections = {
-                    lualine_a = {'mode'},
-                    lualine_b = {'branch',
+                    lualine_a = { "mode" },
+                    lualine_b = {
+                        "branch",
                         {
-                            'diff',
+                            "diff",
                             colored = false, -- Displays a colored diff status if set to true
                             diff_color = {
                                 -- Same color values as the general color option can be used here.
-                                added    = 'DiffAdd',    -- Changes the diff's added color
-                                modified = 'DiffChange', -- Changes the diff's modified color
-                                removed  = 'DiffDelete', -- Changes the diff's removed color you
+                                added = "DiffAdd", -- Changes the diff's added color
+                                modified = "DiffChange", -- Changes the diff's modified color
+                                removed = "DiffDelete", -- Changes the diff's removed color you
                             },
-                            symbols = {added = '+', modified = '~', removed = '-'}, -- Changes the symbols used by the diff.
+                            symbols = { added = "+", modified = "~", removed = "-" }, -- Changes the symbols used by the diff.
                             source = nil, -- A function that works as a data source for diff.
                             -- It must return a table as such:
                             --   { added = add_count, modified = modified_count, removed = removed_count }
                             -- or nil on failure. count <= 0 won't be displayed.
                         },
-                    'diagnostics'},
-                lualine_c = {{'filename', path = 2}},
-                lualine_x = {'encoding', 'fileformat', 'filetype'},
-                lualine_y = {'progress'},
-                    lualine_z = {'location'}
+                        "diagnostics",
+                    },
+                    lualine_c = { { "filename", path = 2 } },
+                    lualine_x = { "encoding", "fileformat", "filetype" },
+                    lualine_y = { "progress" },
+                    lualine_z = { "location" },
                 },
                 inactive_sections = {
                     lualine_a = {},
                     lualine_b = {},
-                    lualine_c = {'filename'},
-                    lualine_x = {'location'},
+                    lualine_c = { "filename" },
+                    lualine_x = { "location" },
                     lualine_y = {},
-                    lualine_z = {}
+                    lualine_z = {},
                 },
                 tabline = {},
                 winbar = {},
                 inactive_winbar = {},
-                extensions = {}
+                extensions = {},
             }
         end,
         event = "VimEnter",
     },
     {
-        'simrat39/symbols-outline.nvim',
+        "simrat39/symbols-outline.nvim",
         config = function()
-            require("symbols-outline").setup{
+            require("symbols-outline").setup {
                 keymaps = {
-                    goto_location = {"<cr>",'<2-LeftMouse>'},
-                }
+                    goto_location = { "<cr>", "<2-LeftMouse>" },
+                },
             }
         end,
-        cmd = {'SymbolsOutline','SymbolsOutlineOpen'},
-    }
+        cmd = { "SymbolsOutline", "SymbolsOutlineOpen" },
+    },
     -- To make a plugin not be loaded
     -- {
     --   "NvChad/nvim-colorizer.lua",
